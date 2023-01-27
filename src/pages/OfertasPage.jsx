cfd10233de14d894188c341be3218de47da47381
hp-Xcangas\Xavier Cangas <xacr06@gmail.com>
2023-01-27 15:42:45 -0500

    agregando animacion paginas

--- src/pages/OfertasPage.jsx
+++ src/pages/OfertasPage.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 
 function OfertasPage() {
   return (
-    <motion.div initial={{opacity:0}} animate={{opacity:1}} exit={{opacity: 0}}>
+    <motion.div initial={{width:0, opacity:0}} animate={{width:"100%", opacity:1}} exit={{x:window.innerWidth, opacity: 0}}>
     <Ofertas/>
     </motion.div>
   )
