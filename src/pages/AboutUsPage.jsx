5cc1e88213f896b830e0b60fb64edc9499481076
hp-Xcangas\Xavier Cangas <xacr06@gmail.com>
2023-02-01 23:07:34 -0500

    modificacion navbar

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -3,6 +3,7 @@ import AboutUs from "../components/AboutUs";
 import React from 'react'
 import {motion} from "framer-motion";
 
+
 function AboutUsPage() {
   return (
     <motion.div initial={{opacity:0}} animate={{opacity:1}} exit={{opacity: 0}}>
