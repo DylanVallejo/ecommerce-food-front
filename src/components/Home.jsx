d95fdc96b43eeff312ac33c60558d075478656ac
DylanVallejo <dylan1811vallejo@gmail.com>
2023-02-03 21:58:36 -0500

    Añadiendo una barra de filtros metodo para filtrar y renderizando la informacion segun la opcion seleccionada

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 // import Carrusel from "./Carrusel"
 import Carrusel2 from "./Carrusel2"
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import '../styles/Home.css'
 import {useNavigate} from 'react-router-dom';
 import Loader from "./Loader";
@@ -8,6 +8,8 @@ import { useSelector, useDispatch } from 'react-redux'
 import { holaReducer , getData } from '../features/data/dataSlice'
 
 function Home () {
+
+    const [barFilter, setBarFilter] = useState("");
     
     const slides = [
         {url:'../img/banner/1.jpg', title : 'Hamburguesa'},    
@@ -36,29 +38,72 @@ function Home () {
         navigate('detalle/'+id)
     }
     
+    
+    const handleFilter = ( filterValue,e ) => {
+        e.preventDefault();
+        setBarFilter(filterValue)
+        console.log(filterValue)
+    }
+    
     return (
         <div>
             {/* <Carrusel/> */}
             <Carrusel2 slides={slides}/>
+            
+            <div className="filterNavigationContainer">
+                <ul className="filterNavigationBarContainer">
+                    <li>
+                        {/* <i className="fa-solid fa-burger"></i> */}
+                        <button onClick={ e => handleFilter("combos",e)} className="listButtonFilterBar">COMBOS</button>
+                    </li>
+                    <li>
+                        {/* <i className="fa-solid fa-burger"></i> */}
+                        <button onClick={ e => handleFilter("hamburguersas",e)} className="listButtonFilterBar">HAMBURGUESAS</button>
+                    </li>
+                    <li>
+                        {/* <i class="fa-solid fa-drumstick"></i> */}
+                        <button onClick={ e => handleFilter("alitas",e) } className="listButtonFilterBar">ALITAS</button>
+                    </li>
+                    <li>
+                        {/* <i class="fa-solid fa-french-fries"></i> */}
+                        <button onClick={ e => handleFilter("papas",e) } className="listButtonFilterBar">PAPAS </button>
+                    </li>
+                    <li>
+                        {/* <i class="fa-solid fa-cup-togo"></i> */}
+                        <button onClick={ e => handleFilter("bebidas",e) } className="listButtonFilterBar">BEBIDAS </button>
+                    </li>
+                    <li>
+                        <button onClick={ e => handleFilter("postres",e) } className="listButtonFilterBar">POSTRES </button>
+                    </li>
+                </ul>
+            </div>
+            
 
             <div className="cardsContainer">
                 {
-                    !loading ? entities.map((item, key)=>{
-                        return(
-                            <div className="mapContainer" key={key}>
-                                <div className="mapImgContainer">
-                                    <img src={item.image} width='80%' height='170em' margin='5px' className="mapImg" alt="ref-home"/>
-                                    <button className="btnDetailsHome" onClick={( e ) => handleNavigation(e,item.id)}>Details</button>
+                    !loading ? entities.filter((val)=>{
+                        if(barFilter === " "){
+                            return val;
+                        }else if(val.category.name.toLowerCase().includes(barFilter.toLowerCase())){
+                            console.log(val.category.name)
+                            return val
+                        }
+                    }).map((item, key)=>{
+                            return(
+                                <div className="mapContainer" key={key}>
+                                    <div className="mapImgContainer">
+                                        <img src={item.image} width='80%' height='170em' margin='5px' className="mapImg" alt="ref-home"/>
+                                        <button className="btnDetailsHome" onClick={( e ) => handleNavigation(e,item.id)}>Details</button>
+                                    </div>
+                                    <div className="mapInfoContainer">
+                                        <h2 className="homeTitle">{item.productName}</h2>
+                                        {/* <p>{item.description.slice(0,30)} ...</p> */}
+                                        <p>{item.price}$</p>
+                                        {/* <p>categorias:{item.category.name}</p> */}
+                                    </div>
                                 </div>
-                                <div className="mapInfoContainer">
-                                    <h2 className="homeTitle">{item.productName}</h2>
-                                    {/* <p>{item.description.slice(0,30)} ...</p> */}
-                                    <p>{item.price}$</p>
-                                    {/* <p>categorias:{item.category.name}</p> */}
-                                </div>
-                            </div>
-                        )
-                    })
+                            )
+                        })
                     : (<Loader/>)
                 }
             </div>
