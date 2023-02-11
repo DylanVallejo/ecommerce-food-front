a5ad49432061a2f25ea833ac2724d4cca36f2119
hp-Xcangas\Xavier Cangas <xacr06@gmail.com>
2023-02-11 17:47:29 -0500

    formulario contact us

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -4,26 +4,28 @@ import styles from '../styles/ContactUs.module.scss'
 function ContactUs() {
   return (
     <div className={styles.contactContainer}>
-        <h2>Ready to connect whit me?</h2>
+      <h2>Ready to connect whit me?</h2>
+      <div className={styles.imgfContainer}>
+        <img src={require("../resources/logofood_k_queso.png")} className={styles.contactUsImg} alt='kruger-img' />
         <form className={styles.formContainer}>
           <h4 >Let's connect</h4>
           <div className={styles.contatcInputForm}>
-            <input type="text" placeholder="Name"  />
+            <input type="text" placeholder="Name" />
             <input type="text" placeholder="Last Name" />
             <input type="email" placeholder="Email Adress" />
-            <textarea 
+            <textarea
               name="story"
-              rows="5" 
+              rows="5"
               cols="33"
               placeholder='Enter text here ...'>
             </textarea>
-            
+
             <button>Get in Touch</button>
           </div>
-          <img src={require("../resources/logofood_k_queso.png")}  className={styles.contactUsImg} alt='kruger-img'/>
         </form>
+      </div>
     </div>
-)
+  )
 }
 
 export default ContactUs
