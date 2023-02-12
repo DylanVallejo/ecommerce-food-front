21762adbf5446645e77dc26610c0bb0f36a2a555
DylanVallejo <dylan1811vallejo@gmail.com>
2023-02-12 18:33:27 -0500

    agregado material ui añadida lista de productos con boton para cambiar stock y oferta agregando logica de navegacion para la compra de productos primero registrarse para agregar al carrito

---
+++ src/components/HandleStore.jsx
@@ -0,0 +1,69 @@
+import {  useState, } from 'react'
+
+import { useSelector,useDispatch } from 'react-redux'
+import axios from 'axios'
+
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import styles from '../styles/HandleStore.module.scss'
+
+function HandleStore() {
+    
+    
+    
+    
+    const [stock, setStock] = useState(0)
+    const [itsInOffers, setItsInOffers] = useState(false)
+    
+    const {entities} = useSelector(state=>state.data)
+    
+    const urlUpdate = `http://localhost:8080/api/product/update/`
+    
+    const handleStock = (stock) => {
+        //traer stock actual mas stock antiguo crear un estado nuevo 
+        const newStock = entities.stock + stock
+        
+        axios.post(urlUpdate,{
+            "stock": stock
+        })
+    }    
+    
+    const setOffer = (oferta) => {
+        
+    }
+    return (
+        <TableContainer className={styles.storeContainer}  sx={{ minWidth: 650 , maxWidth:900}} >
+            <Table 
+            // sx={{ minWidth: 650 }}  
+            className={styles.storeContainerTable}>
+                <TableHead >
+                    <TableRow>
+                        <TableCell className={styles.storeHeaders}>PRODUCTO</TableCell>
+                        <TableCell align="right" className={styles.storeHeaders}>STOCK</TableCell>
+                        <TableCell align="right" className={styles.storeHeaders}>OFERTA</TableCell>
+                        {/* <TableCell align="right">Carbs</TableCell>
+                        <TableCell align="right"></TableCell> */}
+                    </TableRow>
+                </TableHead>
+                <TableBody >
+                    {entities.map((item,key)=>{
+                        return(
+                            <TableRow className={styles.infoTable}>
+                                <TableCell className={styles.infoProducts}  align="left">{item.productName}</TableCell>
+                                <TableCell className={styles.infoProducts} align="right">{item.stock} <button onClick={handleStock(item.stock)}>Agregar Stock</button></TableCell>
+                                <TableCell className={styles.infoProducts} align="right">{item.itsInOffers? <p>SI</p>:<p>NO</p>}<button onClick={setOffer(item.itsInOffers)}>poner en oferta</button></TableCell>
+                            </TableRow>
+                        )
+                    })}
+                </TableBody>
+            
+            </Table>
+        </TableContainer >
+    )
+}
+
+export default HandleStore
