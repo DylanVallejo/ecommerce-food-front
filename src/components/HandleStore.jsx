4586d55212e690bc1c830e545754fb07ec207cc6
hp-Xcangas\Xavier Cangas <xacr06@gmail.com>
2023-02-14 20:07:15 -0500

    cambios table

--- src/components/HandleStore.jsx
+++ src/components/HandleStore.jsx
@@ -1,6 +1,6 @@
-import {  useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 
-import { useSelector,useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import axios from 'axios'
 
 
@@ -13,38 +13,40 @@ import TableRow from '@mui/material/TableRow';
 import styles from '../styles/HandleStore.module.scss'
 import Swal from 'sweetalert2'
 import { getData } from "../features/data/dataSlice";
+import TablePagination from '@mui/material/TablePagination';
+
 
 
 function HandleStore() {
-    
-    
+
+
     const [newStock, setNewStock] = useState(0)
     const [itsInOffers, setItsInOffers] = useState(false)
     const [cambio, setCambio] = useState(0)
     // useState
-    
-    const {entities} = useSelector(state=>state.data)
+
+    const { entities } = useSelector(state => state.data)
     // console.log(entities)
-    
-    
+
+
     const urlUpdate = ` http://localhost:8082/api/product/update/product/`
     // http://localhost:8082/api/product/update/product/13/stock/25
     const dispatch = useDispatch();
-    
-    
+
+
     useEffect(() => {
         // dispatch(getOrderStatus())
-        
+
         dispatch(getData());
         console.log("useeffect")
         // dispatch(createOrder())
-    console.log(cambio)
-        
+        console.log(cambio)
+
 
     }, [cambio]);
-    
-    
-    
+
+
+
     const Toast = Swal.mixin({
         toast: true,
         position: 'top-end',
@@ -56,69 +58,85 @@ function HandleStore() {
             toast.addEventListener('mouseleave', Swal.resumeTimer)
         }
     })
-    
-    const handleStock = (stock,id,e) => {
+
+    const handleStock = (stock, id, e) => {
         e.preventDefault();
         // console.log(stock)
         // console.log(id)
         // console.log(newStock)
-        
+
         const finalUrl = `${urlUpdate}${id}/stock/`
         // console.log(finalUrl)
         let manejarRefresh = cambio
-        
+
         Swal.fire({
-        title: 'Agregar stock',
-        input: 'number',
-        inputAttributes: {
-            autocapitalize: 'off'
-        },
-        showCancelButton: true,
-        confirmButtonText: 'Agregar',
-        showLoaderOnConfirm: true,
-        preConfirm: (createStock) => {
-            // console.log(typeof(createStock))
-            console.log("entrando al post")
-            console.log(`${finalUrl}${stock+ Number(createStock)}`)
-            return axios.put(`${finalUrl}${stock+Number(createStock)}`)
-            .then(response => {
-                console.log("entro al post")
-                console.log(response)
-                console.log(cambio)
-                setCambio(manejarRefresh+1)
-                console.log(cambio)
-                
-                
-                Toast.fire({
-                icon: 'success',
-                title: `${response.data}`
-                })
-            })
-            .catch(error => {
-                Swal.showValidationMessage(
-                `Request failed: ${error}`
-                )
-            })
-        },
-        // allowOutsideClick: () => !Swal.isLoading()
+            title: 'Agregar stock',
+            input: 'number',
+            inputAttributes: {
+                autocapitalize: 'off'
+            },
+            showCancelButton: true,
+            confirmButtonText: 'Agregar',
+            showLoaderOnConfirm: true,
+            preConfirm: (createStock) => {
+                // console.log(typeof(createStock))
+                console.log("entrando al post")
+                console.log(`${finalUrl}${stock + Number(createStock)}`)
+                return axios.put(`${finalUrl}${stock + Number(createStock)}`)
+                    .then(response => {
+                        console.log("entro al post")
+                        console.log(response)
+                        console.log(cambio)
+                        setCambio(manejarRefresh + 1)
+                        console.log(cambio)
+
+
+                        Toast.fire({
+                            icon: 'success',
+                            title: `${response.data}`
+                        })
+                    })
+                    .catch(error => {
+                        Swal.showValidationMessage(
+                            `Request failed: ${error}`
+                        )
+                    })
+            },
+            // allowOutsideClick: () => !Swal.isLoading()
         })
-        
+
         // const newStock = entities.stock
-        
-        
+
+
         // axios.post(`${urlUpdate}/${id}`,{
         //     "stock": stock
         // })
-    }    
-    
+    }
+
     // const setOffer = (oferta) => {
     //     console.log(oferta)
     // }
+
+//paginador de tabla//
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(10);
+  
+    const handleChangePage = (event, newPage) => {
+      setPage(newPage);
+    };
+  
+    const handleChangeRowsPerPage = (event) => {
+      setRowsPerPage(+event.target.value);
+      setPage(0);
+    };
+//theme
+
+
     return (
-        <TableContainer className={styles.storeContainer}  sx={{ minWidth: 650 , maxWidth:900}} >
-            <Table 
-            // sx={{ minWidth: 650 }}  
-            className={styles.storeContainerTable}>
+        <TableContainer className={styles.storeContainer} sx={{ minWidth: 650, maxWidth: 900 }} >
+            <Table stickyHeader aria-label="sticky table"
+                // sx={{ minWidth: 650 }}  
+                className={styles.storeContainerTable}>
                 <TableHead >
                     <TableRow>
                         <TableCell className={styles.storeHeaders}>PRODUCTO</TableCell>
@@ -129,20 +147,30 @@ function HandleStore() {
                     </TableRow>
                 </TableHead>
                 <TableBody >
-                    {entities.map((item,key)=>{
-                        return(
+                    {entities.slice
+                    (page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((item, key) => {
+                        return (
                             <TableRow className={styles.infoTable} key={key}>
 
-                                <TableCell className={styles.infoProducts}  align="center">{item.productName}</TableCell>
-                                <TableCell className={styles.infoProducts} align="center"><p>{item.stock}</p> <button onClick={e=>handleStock(item.stock,item.id,e)}>Agregar Stock</button></TableCell>
-                                <TableCell className={styles.infoProducts} align="center">{item.itsInOffers? <p>SI</p>:<p>NO</p>}<button onClick={()=>{}}>Poner en oferta</button></TableCell>
+                                <TableCell className={styles.infoProducts} align="center">{item.productName}</TableCell>
+                                <TableCell className={styles.infoProducts} align="center"><p>{item.stock}</p> <button onClick={e => handleStock(item.stock, item.id, e)}>Agregar Stock</button></TableCell>
+                                <TableCell className={styles.infoProducts} align="center">{item.itsInOffers ? <p>SI</p> : <p>NO</p>}<button onClick={() => { }}>Poner en oferta</button></TableCell>
 
                             </TableRow>
                         )
                     })}
                 </TableBody>
-            
+
             </Table>
+            <TablePagination
+                rowsPerPageOptions={[10, 25, 100]}
+                component="div"
+                count={entities.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+            />
         </TableContainer >
     )
 }
